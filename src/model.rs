6affc2a46d60ecd0b4021cc828fe08c529e38426
Daniel Way <projects@danieldway.com>
2023-08-20 10:07:57 -0500

    Create message 31 data header definition

--- src/model.rs
+++ src/model.rs
@@ -67,3 +67,66 @@ pub struct MessageHeader {
     /// Number of this segment
     pub seg_num: u16,
 }
+
+#[repr(C)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DataHeader {
+    /// Radar iste identifier
+    pub radar_id: [u8; 4],
+
+    /// Data collection time in milliseconds past midnight GMT
+    pub ray_time: u32,
+
+    /// Julian date - 2440586.5 (1/01/1970)
+    pub ray_date: u16,
+
+    /// Radial number within elevation scan
+    pub azm_num: u16,
+
+    /// Azimuth angle in degrees (0 to 359.956055)
+    pub azm: f32,
+
+    /// 0 = uncompressed, 1 = BZIP2, 2 = zlib
+    pub compression_code: u8,
+
+    /// For word alignment
+    pub spare: u8,
+
+    /// Radial length in bytes, including data header block
+    pub radial_len: u16,
+
+    /// Azimuthal resolution
+    pub azm_res: u8,
+
+    /// Radial status
+    pub radial_status: u8,
+
+    /// Elevation number
+    pub elev_num: u8,
+
+    /// Sector cut number
+    pub sector_cut_num: u8,
+
+    /// Elevation angle in degrees (-7.0 to 70.0)
+    pub elev: f32,
+
+    /// Radial spot blanking
+    pub radial_spot_blanking: u8,
+
+    /// Azimuth indexing mode
+    pub azm_indexing_mode: u8,
+
+    /// Data block count
+    pub data_block_count: u16,
+
+    /// Data block pointers
+    pub vol_const: u32,
+    pub elev_const: u32,
+    pub radial_const: u32,
+    pub field1: u32,
+    pub field2: u32,
+    pub field3: u32,
+    pub field4: u32,
+    pub field5: u32,
+    pub field6: u32,
+}

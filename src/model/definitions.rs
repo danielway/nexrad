7fed07313066b4269e9289a3c0b76af9c1218803
Daniel Way <projects@danieldway.com>
2024-04-22 20:05:00 -0500

    Define general message header

---
+++ src/model/definitions.rs
@@ -0,0 +1,9 @@
+/// The possible RDA redundant channels.
+pub enum RedundantChannel {
+    LegacySingleChannel,
+    LegacyRedundantChannel1,
+    LegacyRedundantChannel2,
+    ORDASingleChannel,
+    ORDARedundantChannel1,
+    ORDARedundantChannel2,
+}

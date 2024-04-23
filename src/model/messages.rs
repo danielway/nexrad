870ae25796a4e93ad2d714b8782e1669d3077c8f
Daniel Way <projects@danieldway.com>
2024-04-22 20:39:49 -0500

    Define digital radar data module

--- src/model/messages.rs
+++ src/model/messages.rs
@@ -1,2 +1,5 @@
 mod rda_status_data;
 pub use rda_status_data::*;
+
+mod digital_radar_data;
+pub use digital_radar_data::*;
---
+++ src/model/messages/digital_radar_data.rs
@@ -0,0 +1,9 @@
+//!
+//! Message type 31 "Digital Radar Data" consists of base data information such as reflectivity,
+//! mean radial velocity, spectrum width, differential reflectivity, differential phase, correlation
+//! coefficient, azimuth angle, elevation angle, cut type, scanning strategy, and calibration
+//! parameters. The frequency and volume of the message is dependent on the scanning strategy and
+//! the type of data associated with that strategy.
+//!
+
+mod data_header;

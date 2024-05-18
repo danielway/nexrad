7845de487744ac63c0bb7190bfb6f711b8f9d5cf
Daniel Way <projects@danieldway.com>
2024-05-17 19:40:38 -0500

    Define radar product enumeration

---
+++ src/product.rs
@@ -0,0 +1,10 @@
+/// Renderable radar data products.
+pub enum Product {
+    Reflectivity,
+    Velocity,
+    SpectrumWidth,
+    DifferentialReflectivity,
+    DifferentialPhase,
+    CorrelationCoefficient,
+    SpecificDiffPhase,
+}

5c356ea6b6e7704e31fd3cb53e5f1fda5e057a16
Daniel Way <projects@danieldway.com>
2024-06-23 18:31:32 -0500

    Convert to `thiserror`

--- src/result.rs
+++ src/result.rs
@@ -2,38 +2,26 @@
 //! Contains the Result and Error types for NEXRAD operations.
 //!
 
+use thiserror::Error as ThisError;
+
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(ThisError, Debug)]
 pub enum Error {
-    DecompressionError(String),
-    FileError(std::io::Error),
-    DeserializationError(bincode::Error),
+    #[error("data file IO error")]
+    FileError(#[from] std::io::Error),
+    #[error("file deserialization error")]
+    DeserializationError(#[from] bincode::Error),
     #[cfg(feature = "download")]
-    S3GeneralError(aws_sdk_s3::Error),
+    #[error("AWS API error")]
+    S3GeneralError(#[from] aws_sdk_s3::Error),
     #[cfg(feature = "download")]
+    #[error("error listing AWS S3 objects")]
     S3ListObjectsError,
     #[cfg(feature = "download")]
+    #[error("error getting AWS S3 object")]
     S3GetObjectError,
     #[cfg(feature = "download")]
+    #[error("error streaming/downloading AWS S3 object")]
     S3StreamingError,
-}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Error::FileError(err)
-    }
-}
-
-impl From<bincode::Error> for Error {
-    fn from(err: bincode::Error) -> Self {
-        Error::DeserializationError(err)
-    }
-}
-
-#[cfg(feature = "download")]
-impl From<aws_sdk_s3::Error> for Error {
-    fn from(err: aws_sdk_s3::Error) -> Self {
-        Error::S3GeneralError(err)
-    }
 }

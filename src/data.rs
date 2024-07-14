c2fe4df478f2865ca3674f0948b9ed6e994d342c
Daniel Way <projects@danieldway.com>
2024-07-14 12:37:45 -0500

    Define `data` module

---
+++ src/data.rs
@@ -0,0 +1,9 @@
+//!
+//! This module contains models representing digital radar data collected by the NEXRAD weather
+//! radar network. These models and their APIs are intended to be ergonomic, understandable, and
+//! performant. They do not exactly match the encoded structure from common archival formats.
+//!
+//! todo: note optional uom interfaces
+//! todo: note serialization support
+//! todo: note filtering capability
+//!

68ff329857b5ae7d5be8d61a3e291d6df2a6e119
Daniel Way <projects@danieldway.com>
2024-07-17 19:28:08 -0500

    Add accessor for site latitude/longitude

--- src/meta.rs
+++ src/meta.rs
@@ -4,6 +4,9 @@
 //! is represented separately to avoid duplication in storage.
 //!
 
+use uom::si::angle::degree;
+use uom::si::f32::Angle;
+
 /// A radar site's metadata including a variety of infrequently-changing properties.
 pub struct Site {
     identifier: [u8; 4],
@@ -22,5 +25,27 @@ impl Site {
     /// The four-letter ICAO identifier for the radar site as a string.
     pub fn identifier_string(&self) -> String {
         String::from_utf8_lossy(&self.identifier).to_string()
+    }
+
+    /// The latitude of the radar site in degrees.
+    pub fn latitude_degrees(&self) -> f32 {
+        self.latitude
+    }
+
+    /// The latitude of the radar site.
+    #[cfg(feature = "uom")]
+    pub fn latitude(&self) -> Angle {
+        Angle::new::<degree>(self.latitude)
+    }
+
+    /// The longitude of the radar site in degrees.
+    pub fn longitude_degrees(&self) -> f32 {
+        self.longitude
+    }
+
+    /// The longitude of the radar site.
+    #[cfg(feature = "uom")]
+    pub fn longitude(&self) -> Angle {
+        Angle::new::<degree>(self.longitude)
     }
 }

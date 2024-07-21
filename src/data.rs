02e182c05795db3cb4ce0c304dc97e87e87d556c
Daniel Way <projects@danieldway.com>
2024-07-21 08:25:48 -0500

    Implement the scan structure

--- src/data.rs
+++ src/data.rs
@@ -20,7 +20,22 @@ use uom::si::{
 /// is composed of multiple sweeps at different elevations. The pattern of sweeps, including
 /// elevations and resolution, is determined by the scanning strategy of the radar. This is
 /// referred to as the Volume Coverage Pattern.
-pub struct Scan;
+pub struct Scan {
+    coverage_pattern_number: u16,
+    sweeps: Vec<Sweep>,
+}
+
+impl Scan {
+    /// This scan's volume coverage pattern number.
+    pub fn coverage_pattern_number(&self) -> u16 {
+        self.coverage_pattern_number
+    }
+
+    /// The elevation sweeps comprising this scan.
+    pub fn sweeps(&self) -> &Vec<Sweep> {
+        self.sweeps.as_ref()
+    }
+}
 
 /// A single radar sweep composed of a series of radials. This represents a full rotation of the
 /// radar at some elevation angle and contains the Level II data (reflectivity, velocity, and

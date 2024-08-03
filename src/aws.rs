640650bca4247349c2aa481e393a3fd63b102799
Daniel Way <projects@danieldway.com>
2024-08-03 06:51:33 -0500

    Restructure archive into its own directory

--- src/aws.rs
+++ src/aws.rs
@@ -2,6 +2,8 @@
 //! Downloads NEXRAD level-II data from an AWS S3 bucket populated by NOAA.
 //!
 
+pub mod archive;
+
 use chrono::{DateTime, NaiveDate, Utc};
 use xml::reader::XmlEvent;
 use xml::EventReader;

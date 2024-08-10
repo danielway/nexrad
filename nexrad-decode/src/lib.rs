a218893428f092158b5be5ca3e9f637c072586c1
Daniel Way <projects@danieldway.com>
2024-08-10 10:13:05 -0500

    Create nexrad-decode crate

---
+++ nexrad-decode/src/lib.rs
@@ -0,0 +1,15 @@
+//!
+//! # nexrad-decode
+//! Decoding functions and models for NEXRAD weather radar data. Decoder and struct definitions are
+//! in accordance with NOAA's WSR-88D Interface Control Document for Archive II "ICD 2620010H"
+//! build 19.0.
+//!
+//! Optionally, the `nexrad-model` feature provides mappings to a common model for representing
+//! radar data. The `uom` feature can also be used to provide type-safe units of measure.
+//!
+
+#![forbid(unsafe_code)]
+#![warn(clippy::correctness)]
+#![allow(clippy::too_many_arguments)]
+
+pub mod result;

2df978ab712fd9d6f6d25fe57ac887e328c05db1
Daniel Way <projects@danieldway.com>
2024-08-11 21:12:33 -0500

    Delete old/moved code from `nexrad`

--- nexrad/src/lib.rs
+++ nexrad/src/lib.rs
@@ -6,14 +6,4 @@
 //! Download and decode functions for NEXRAD radar data.
 //!
 
-extern crate core;
-
-pub mod decode;
-pub mod file;
 pub mod result;
-
-#[cfg(any(feature = "decompress", feature = "decompress-wasm"))]
-pub mod decompress;
-
-#[cfg(feature = "download")]
-pub mod download;
--- nexrad/src/result.rs
+++ nexrad/src/result.rs
@@ -7,18 +7,4 @@ use thiserror::Error as ThisError;
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(ThisError, Debug)]
-pub enum Error {
-    #[error("data file IO error")]
-    FileError(#[from] std::io::Error),
-    #[error("file deserialization error")]
-    DeserializationError(#[from] bincode::Error),
-    #[cfg(feature = "download")]
-    #[error("error listing AWS S3 objects")]
-    S3ListObjectsError(reqwest::Error),
-    #[cfg(feature = "download")]
-    #[error("error getting AWS S3 object")]
-    S3GetObjectError(reqwest::Error),
-    #[cfg(feature = "download")]
-    #[error("error streaming/downloading AWS S3 object")]
-    S3StreamingError(reqwest::Error),
-}
+pub enum Error {}

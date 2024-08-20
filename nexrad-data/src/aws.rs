ca7c779395e7e6d6382be88dcc94900f5db25421
Daniel Way <projects@danieldway.com>
2024-08-19 19:11:18 -0500

    Move AWS documentation to module

--- nexrad-data/src/aws.rs
+++ nexrad-data/src/aws.rs
@@ -1,5 +1,14 @@
 //!
-//! Downloads NEXRAD level-II data from an AWS S3 bucket populated by NOAA.
+//! Downloads archival and real-time NEXRAD level II weather radar data from AWS Open Data buckets
+//! populated by NOAA.
+//!
+//! [AWS Open Data NOAA NEXRAD](https://registry.opendata.aws/noaa-nexrad/)
+//!
+//! [AWS Labs Open Data Documentation](https://github.com/awslabs/open-data-docs/tree/main/docs/noaa/noaa-nexrad)
+//!
+//! **NEXRAD Level II archive data**: `arn:aws:s3:::noaa-nexrad-level2`
+//!
+//! **NEXRAD Level II real-time data**: `arn:aws:s3:::unidata-nexrad-level2-chunks`
 //!
 
 pub mod archive;

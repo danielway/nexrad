973f4c1e32a8b736f3c4a8c7e0dd7259034ae88f
Daniel Way <projects@danieldway.com>
2024-09-04 20:59:43 -0500

    Delete `get_next_chunk`, update example

--- nexrad-data/examples/download_realtime.rs
+++ nexrad-data/examples/download_realtime.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
-use nexrad_data::aws::realtime::list_chunks_in_volume;
 #[cfg(feature = "aws")]
-use nexrad_data::aws::realtime::{download_chunk, get_latest_volume, get_next_chunk};
+use nexrad_data::aws::realtime::{download_chunk, get_latest_volume};
+use nexrad_data::aws::realtime::{list_chunks_in_volume, ChunkIdentifier, NextChunk, VolumeIndex};
 
 #[cfg(not(feature = "aws"))]
 fn main() {
@@ -25,28 +25,24 @@ async fn main() -> nexrad_data::result::Result<()> {
     let mut downloaded_chunks = 0;
     let mut next_chunk = latest_chunk.clone();
     loop {
-        next_chunk = get_next_chunk("KDMX", next_chunk.clone()).expect("No next chunk found");
-        println!("Next chunk: {:?}", next_chunk);
-
-        for attempt in 0..5 {
-            println!("  Downloading chunk, attempt {}/5...", attempt + 1);
-            match download_chunk("KDMX", &next_chunk).await {
-                Ok(chunk) => {
-                    println!("  Downloaded chunk: {} bytes", chunk.data().len());
-
-                    downloaded_chunks += 1;
-                    break;
+        match next_chunk.next_chunk() {
+            Some(next) => match next {
+                NextChunk::Sequence(next_chunk_identifier) => {
+                    println!("Next chunk: {:?}", next_chunk_identifier);
+                    next_chunk = next_chunk_identifier;
                 }
-                Err(e) => {
-                    println!("Error downloading chunk: {:?}", e);
-                    if attempt == 4 {
-                        return Err(e);
-                    }
+                NextChunk::Volume(next_volume) => {
+                    println!("Next volume: {:?}", next_volume);
+                    next_chunk = attempt_next_volume(next_volume).await?;
                 }
+            },
+            None => {
+                println!("Unable to determine next chunk.");
+                break;
             }
-
-            tokio::time::sleep(Duration::from_secs(2)).await;
         }
+
+        attempt_chunk_download(&next_chunk).await?;
 
         if downloaded_chunks >= 5 {
             break;
@@ -56,4 +52,64 @@ async fn main() -> nexrad_data::result::Result<()> {
     println!("Done!");
 
     Ok(())
+}
+
+#[cfg(feature = "aws")]
+async fn attempt_next_volume(next_volume: VolumeIndex) -> nexrad_data::result::Result<ChunkIdentifier> {
+    for attempt in 0..5 {
+        println!(
+            "  Looking for chunks in volume {:?}, attempt {}/5...",
+            next_volume,
+            attempt + 1
+        );
+        match list_chunks_in_volume("KDMX", next_volume, 100).await {
+            Ok(chunks) => match chunks.last() {
+                Some(chunk) => {
+                    println!("    Found latest chunk: {:?}", chunk);
+                    return Ok(chunk.clone());
+                }
+                None => {
+                    println!("    No chunks found in volume.");
+                    continue;
+                }
+            },
+            Err(e) => {
+                println!("    Error downloading chunk: {:?}", e);
+                if attempt == 4 {
+                    return Err(e);
+                }
+            }
+        }
+
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
+
+    panic!("Unable to find chunk in volume");
+}
+
+#[cfg(feature = "aws")]
+async fn attempt_chunk_download(next_chunk: &ChunkIdentifier) -> nexrad_data::result::Result<()> {
+    for attempt in 0..5 {
+        println!(
+            "  Downloading chunk {}, attempt {}/5...",
+            next_chunk.name(),
+            attempt + 1
+        );
+        match download_chunk("KDMX", &next_chunk).await {
+            Ok(chunk) => {
+                println!("    Downloaded chunk: {} bytes", chunk.data().len());
+                return Ok(());
+            }
+            Err(e) => {
+                println!("    Error downloading chunk: {:?}", e);
+                if attempt == 4 {
+                    return Err(e);
+                }
+            }
+        }
+
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
+
+    panic!("Unable to download chunk");
 }
--- nexrad-data/src/aws/realtime.rs
+++ nexrad-data/src/aws/realtime.rs
@@ -48,9 +48,6 @@ pub use get_latest_volume::*;
 mod list_chunks_in_volume;
 pub use list_chunks_in_volume::*;
 
-mod get_next_chunk;
-pub use get_next_chunk::*;
-
 mod search;
 
 const REALTIME_BUCKET: &str = "unidata-nexrad-level2-chunks";

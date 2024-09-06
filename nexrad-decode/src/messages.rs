dc6a4fe4d1eba65a4b0d1d424ecd02989d0b3c4a
Daniel Way <projects@danieldway.com>
2024-09-06 13:57:39 -0500

    Add multi-message decoding API

--- nexrad-decode/src/messages.rs
+++ nexrad-decode/src/messages.rs
@@ -3,22 +3,45 @@ pub mod digital_radar_data;
 pub mod message_header;
 pub mod rda_status_data;
 
-mod definitions;
-mod primitive_aliases;
-
 mod message_type;
-
 pub use message_type::MessageType;
-use std::io::Read;
 
 mod message;
 pub use message::{Message, MessageWithHeader};
 
+mod definitions;
+mod primitive_aliases;
+
+use crate::messages::clutter_filter_map::decode_clutter_filter_map;
+use crate::messages::digital_radar_data::decode_digital_radar_data;
 use crate::messages::message_header::MessageHeader;
+use crate::messages::rda_status_data::decode_rda_status_message;
 use crate::result::Result;
 use crate::util::deserialize;
+use std::io::{Read, Seek};
 
 /// Decode a NEXRAD Level II message from a reader.
 pub fn decode_message_header<R: Read>(reader: &mut R) -> Result<MessageHeader> {
     deserialize(reader)
+}
+
+/// Decode a series of NEXRAD Level II messages from a reader.
+pub fn decode_messages<R: Read + Seek>(reader: &mut R) -> Result<Vec<MessageWithHeader>> {
+    let mut messages = Vec::new();
+    while let Ok(header) = decode_message_header(reader) {
+        let message = match header.message_type() {
+            MessageType::RDAStatusData => {
+                Message::RDAStatusData(Box::new(decode_rda_status_message(reader)?))
+            }
+            MessageType::RDADigitalRadarDataGenericFormat => {
+                Message::DigitalRadarData(Box::new(decode_digital_radar_data(reader)?))
+            }
+            MessageType::RDAClutterFilterMap => {
+                Message::ClutterFilterMap(Box::new(decode_clutter_filter_map(reader)?))
+            }
+            _ => Message::Other,
+        };
+        messages.push(MessageWithHeader { header, message });
+    }
+    Ok(messages)
 }

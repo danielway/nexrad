807b7c653bbc9e3c25af33a834c4c85d3c3aea4a
Daniel Way <projects@danieldway.com>
2024-09-06 07:33:24 -0500

    Simplify chunk time estimation

--- nexrad-data/src/aws/realtime/estimate_next_chunk_time.rs
+++ nexrad-data/src/aws/realtime/estimate_next_chunk_time.rs
@@ -1,18 +1,21 @@
 use crate::aws::realtime::ChunkIdentifier;
-use chrono::{DateTime, TimeDelta, Utc};
+use chrono::{DateTime, Utc};
 use std::ops::Add;
+use std::time::Duration;
 
-/// Attempts to estimate the time at which the next chunk will be available given the previous.
-pub fn estimate_next_chunk_time(previous_chunk: &ChunkIdentifier) -> Option<DateTime<Utc>> {
-    match (previous_chunk.sequence(), previous_chunk.date_time()) {
-        (Some(sequence), Some(date_time)) => {
-            return match sequence + 1 {
-                1..=7 | 14..=19 | 26..=31 => Some(date_time.add(TimeDelta::seconds(12))),
-                _ => Some(date_time.add(TimeDelta::seconds(4))),
+/// Attempts to estimate the time at which the next chunk will be available given the previous chunk.
+/// A None result indicates that the chunk should already be available.
+pub fn estimate_next_chunk_time(previous_chunk: &ChunkIdentifier) -> DateTime<Utc> {
+    let mut estimated_wait_time = Duration::from_secs(4);
+    if let Some(sequence) = previous_chunk.sequence() {
+        match sequence + 1 {
+            1..=7 | 14..=19 | 26..=31 => {
+                estimated_wait_time = Duration::from_secs(12);
             }
-        }
-        _ => {}
+            _ => {}
+        };
     }
 
-    None
+    let previous_time = previous_chunk.date_time().unwrap_or_else(|| Utc::now());
+    previous_time.add(Duration::from_millis(estimated_wait_time.as_millis() as u64))
 }

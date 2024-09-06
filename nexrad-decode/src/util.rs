9e21b91ab4b6a33c55df600a27e9abd1a02eb195
Daniel Way <projects@danieldway.com>
2024-09-06 09:58:49 -0500

    Fix Julian time day off-by-one

--- nexrad-decode/src/util.rs
+++ nexrad-decode/src/util.rs
@@ -11,7 +11,7 @@ pub(crate) fn get_datetime(
     past_midnight: Duration,
 ) -> Option<DateTime<Utc>> {
     let count_start = NaiveDate::from_ymd_opt(1970, 1, 1)?;
-    let date = count_start + Duration::days(modified_julian_date as i64);
+    let date = count_start + Duration::days(modified_julian_date as i64 - 1);
     let time = NaiveTime::from_num_seconds_from_midnight_opt(0, 0)? + past_midnight;
 
     Some(DateTime::from_naive_utc_and_offset(

6f417e925b850d7d24c2fa3ddee2faf75956a7df
Daniel Way <projects@danieldway.com>
2024-09-11 21:09:38 -0500

    Adjust segmented/fixed-size decoding logic

--- nexrad-decode/src/messages.rs
+++ nexrad-decode/src/messages.rs
@@ -29,19 +29,33 @@ pub fn decode_message_header<R: Read>(reader: &mut R) -> Result<MessageHeader> {
 pub fn decode_messages<R: Read + Seek>(reader: &mut R) -> Result<Vec<MessageWithHeader>> {
     let mut messages = Vec::new();
     while let Ok(header) = decode_message_header(reader) {
-        let message = match header.message_type() {
-            MessageType::RDAStatusData => {
-                Message::RDAStatusData(Box::new(decode_rda_status_message(reader)?))
-            }
-            MessageType::RDADigitalRadarDataGenericFormat => {
-                Message::DigitalRadarData(Box::new(decode_digital_radar_data(reader)?))
-            }
-            MessageType::RDAClutterFilterMap => {
-                Message::ClutterFilterMap(Box::new(decode_clutter_filter_map(reader)?))
-            }
-            _ => Message::Other,
-        };
+        let message = decode_message(reader, header.message_type())?;
         messages.push(MessageWithHeader { header, message });
     }
     Ok(messages)
+}
+
+/// Decode a NEXRAD Level II message of the specified type from a reader.
+pub fn decode_message<R: Read + Seek>(
+    reader: &mut R,
+    message_type: MessageType,
+) -> Result<Message> {
+    if message_type == MessageType::RDADigitalRadarDataGenericFormat {
+        let decoded_message = decode_digital_radar_data(reader)?;
+        return Ok(Message::DigitalRadarData(Box::new(decoded_message)));
+    }
+
+    let mut message_buffer = [0; 2432 - size_of::<MessageHeader>()];
+    reader.read_exact(&mut message_buffer)?;
+
+    let message_reader = &mut message_buffer.as_ref();
+    Ok(match message_type {
+        MessageType::RDAStatusData => {
+            Message::RDAStatusData(Box::new(decode_rda_status_message(message_reader)?))
+        }
+        MessageType::RDAClutterFilterMap => {
+            Message::ClutterFilterMap(Box::new(decode_clutter_filter_map(message_reader)?))
+        }
+        _ => Message::Other,
+    })
 }

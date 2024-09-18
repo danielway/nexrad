5297a79c6f567f2440b709ccbbe882597f7bc1ad
Daniel Way <projects@danieldway.com>
2024-09-17 21:28:58 -0500

    Update crate documentation

--- nexrad-data/src/lib.rs
+++ nexrad-data/src/lib.rs
@@ -1,6 +1,8 @@
 //!
 //! # nexrad-data
-//! Download and processing functions for NEXRAD weather radar data.
+//! Provides structure definitions and decoding functions for NEXRAD Archive II volume files, along
+//! with functions for downloading both archival and real-time data from open cloud providers like
+//! AWS OpenData.
 //!
 
 #![forbid(unsafe_code)]

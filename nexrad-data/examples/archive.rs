57096db5174f1b472b91378473a09fed2e7d48db
Daniel Way <projects@danieldway.com>
2024-09-18 22:26:34 -0500

    Update `nexrad-data` examples to use summarization API

--- nexrad-data/examples/archive.rs
+++ nexrad-data/examples/archive.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use log::{debug, info, trace, LevelFilter};
+use log::{debug, info, trace, warn, LevelFilter};
 
 #[cfg(not(all(feature = "aws", feature = "decode")))]
 fn main() {
@@ -29,7 +29,6 @@ struct Cli {
 #[cfg(all(feature = "aws", feature = "decode"))]
 #[tokio::main]
 async fn main() -> nexrad_data::result::Result<()> {
-    use chrono::Utc;
     use chrono::{NaiveDate, NaiveTime};
     use nexrad_data::aws::archive::{download_file, list_files};
     use nexrad_data::volume::File;
@@ -50,39 +49,38 @@ async fn main() -> nexrad_data::result::Result<()> {
         NaiveTime::parse_from_str(&cli.start_time, "%H:%M").expect("start is valid time");
     let stop_time = NaiveTime::parse_from_str(&cli.stop_time, "%H:%M").expect("stop is valid time");
 
-    println!("Listing files for {} on {}...", site, date);
+    info!("Listing files for {} on {}...", site, date);
     let file_ids = list_files(site, &date).await?;
 
     if file_ids.is_empty() {
-        println!("No files found for the specified date/site to download.");
+        warn!("No files found for the specified date/site to download.");
         return Ok(());
     }
 
-    println!("Found {} files.", file_ids.len());
+    debug!("Found {} files.", file_ids.len());
 
     let start_index = get_nearest_file_index(&file_ids, start_time);
-    println!(
+    debug!(
         "Nearest file to start of {:?} is {:?}.",
         start_time,
         file_ids[start_index].name()
     );
 
     let stop_index = get_nearest_file_index(&file_ids, stop_time);
-    println!(
+    debug!(
         "Nearest file to stop of {:?} is {:?}.",
         stop_time,
         file_ids[stop_index].name()
     );
 
-    println!("Downloading {} files...", stop_index - start_index + 1);
-
+    debug!("Downloading {} files...", stop_index - start_index + 1);
     for file_id in file_ids
         .iter()
         .skip(start_index)
         .take(stop_index - start_index + 1)
     {
         let file = if Path::new(&format!("downloads/{}", file_id.name())).exists() {
-            println!("File \"{}\" already downloaded.", file_id.name());
+            debug!("File \"{}\" already downloaded.", file_id.name());
             let mut file =
                 std::fs::File::open(format!("downloads/{}", file_id.name())).expect("open file");
 
@@ -91,21 +89,15 @@ async fn main() -> nexrad_data::result::Result<()> {
 
             File::new(buffer)
         } else {
-            println!("Downloading file \"{}\"...", file_id.name());
+            debug!("Downloading file \"{}\"...", file_id.name());
             let file = download_file(file_id.clone()).await?;
 
-            println!("Data file size (bytes): {}", file.data().len());
             if !Path::new("downloads").exists() {
-                println!("Creating downloads directory...");
+                trace!("Creating downloads directory...");
                 create_dir("downloads").expect("create downloads directory");
             }
 
-            if !Path::new("downloads").exists() {
-                println!("Creating downloads directory...");
-                create_dir("downloads").expect("create downloads directory");
-            }
-
-            println!("Writing file to disk as: {}", file_id.name());
+            trace!("Writing file to disk as: {}", file_id.name());
             let mut downloaded_file =
                 std::fs::File::create(format!("downloads/{}", file_id.name()))
                     .expect("create file");
@@ -117,21 +109,29 @@ async fn main() -> nexrad_data::result::Result<()> {
             file
         };
 
-        println!("Data file size (bytes): {}", file.data().len());
+        trace!("Data file size (bytes): {}", file.data().len());
 
         let records = file.records();
         debug!(
-            "Volume start chunk with {} records. Header: {:?}",
+            "Volume with {} records. Header: {:?}",
             records.len(),
             file.header()
         );
 
-        records
-            .into_iter()
-            .for_each(|record| decode_record(record, Utc::now()));
-    }
+        let mut messages = Vec::new();
+        for mut record in records {
+            debug!("Decoding record...");
+            if record.compressed() {
+                trace!("Decompressing LDM record...");
+                record = record.decompress().expect("Failed to decompress record");
+            }
 
-    println!("Downloaded {} files.", stop_index - start_index + 1);
+            messages.extend(record.messages()?.iter().cloned());
+        }
+
+        let summary = nexrad_decode::summarize::messages(messages.as_slice());
+        info!("Volume summary:\n{:#?}", summary);
+    }
 
     Ok(())
 }
@@ -167,143 +167,4 @@ fn get_nearest_file_index(
     }
 
     min_index
-}
-
-#[cfg(all(feature = "aws", feature = "decode"))]
-fn decode_record(
-    mut record: nexrad_data::volume::Record,
-    download_time: chrono::DateTime<chrono::Utc>,
-) {
-    use nexrad_decode::messages::MessageType;
-    use nexrad_decode::messages::{decode_messages, Message};
-    use std::collections::HashMap;
-    use std::io::Cursor;
-
-    if record.compressed() {
-        trace!("Decompressing LDM record...");
-        record = record.decompress().expect("Failed to decompress record");
-    } else {
-        trace!("Decompressed LDM record");
-    }
-
-    let mut message_type_counts = HashMap::new();
-
-    let mut all_scans = Vec::new();
-    let mut first_message_time = None;
-    let mut coverage_pattern = None;
-    let mut scan_data: Option<ScanData> = None;
-
-    let mut reader = Cursor::new(record.data());
-    let messages = decode_messages(&mut reader).expect("Failed to decode messages");
-    for message in messages {
-        let message_header = message.header;
-
-        if first_message_time.is_none() {
-            first_message_time = message_header.date_time();
-        }
-
-        let message_type = message_header.message_type();
-        let count = message_type_counts.get(&message_type).unwrap_or(&0) + 1;
-        message_type_counts.insert(message_type, count);
-
-        match message.message {
-            Message::DigitalRadarData(m31) => {
-                if coverage_pattern.is_none() {
-                    coverage_pattern = Some(
-                        m31.volume_data_block
-                            .expect("No volume data block")
-                            .volume_coverage_pattern_number,
-                    );
-                }
-
-                if let Some(current_scan_data) = scan_data.as_mut() {
-                    if current_scan_data.elevation == m31.header.elevation_number {
-                        current_scan_data.end_azimuth = m31.header.azimuth_angle;
-
-                        let mut increment_count = |data_type: &str| {
-                            let count =
-                                current_scan_data.data_types.get(data_type).unwrap_or(&0) + 1;
-                            current_scan_data
-                                .data_types
-                                .insert(data_type.to_string(), count);
-                        };
-
-                        if m31.reflectivity_data_block.is_some() {
-                            increment_count("Reflectivity");
-                        }
-                        if m31.velocity_data_block.is_some() {
-                            increment_count("Velocity");
-                        }
-                        if m31.spectrum_width_data_block.is_some() {
-                            increment_count("Spectrum Width");
-                        }
-                        if m31.differential_reflectivity_data_block.is_some() {
-                            increment_count("Differential Reflectivity");
-                        }
-                        if m31.differential_phase_data_block.is_some() {
-                            increment_count("Differential Phase");
-                        }
-                        if m31.correlation_coefficient_data_block.is_some() {
-                            increment_count("Correlation Coefficient");
-                        }
-                        if m31.specific_diff_phase_data_block.is_some() {
-                            increment_count("Specific Differential Phase");
-                        }
-                    } else {
-                        all_scans.push(format!("{}", current_scan_data));
-                        scan_data = None;
-                    }
-                }
-
-                if scan_data.is_none() {
-                    scan_data = Some(ScanData {
-                        start_azimuth: m31.header.azimuth_angle,
-                        end_azimuth: m31.header.azimuth_angle,
-                        elevation: m31.header.elevation_number,
-                        data_types: HashMap::new(),
-                    });
-                }
-            }
-            _ => {
-                if let Some(scan_data) = scan_data.take() {
-                    all_scans.push(format!("{}", scan_data));
-                }
-            }
-        }
-    }
-
-    if let Some(scan_info) = scan_data.take() {
-        all_scans.push(format!("{}", scan_info));
-    }
-
-    debug!(
-        "Message latency: {:?}, Coverage pattern: {:?}",
-        first_message_time.map(|time| download_time - time),
-        coverage_pattern
-    );
-
-    for (message_type, count) in message_type_counts {
-        debug!("Message type {:?} has {} messages", message_type, count);
-
-        if message_type == MessageType::RDADigitalRadarDataGenericFormat {
-            info!("{}", all_scans.join(", "));
-        }
-    }
-}
-
-struct ScanData {
-    start_azimuth: f32,
-    end_azimuth: f32,
-    elevation: u8,
-    data_types: std::collections::HashMap<String, usize>,
-}
-
-impl std::fmt::Display for ScanData {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "ScanData {{ azimuth: {:.0}-{:.0}, elevation: {}, data_types: {:?} }}",
-            self.start_azimuth, self.end_azimuth, self.elevation, self.data_types
-        )
-    }
 }

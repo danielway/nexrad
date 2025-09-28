420b356cd62298e6ac8a48588910c11b687425c0
Daniel Way <projects@danieldway.com>
2025-09-28 20:37:22 +0000

    Add test for volume header decoding

--- nexrad-decode/tests/volume_records.rs
+++ nexrad-decode/tests/volume_records.rs
@@ -3,7 +3,7 @@ use nexrad_data::volume;
 const TEST_NEXRAD_FILE: &[u8] = include_bytes!("KDMX20220305_232324_V06");
 
 #[test]
-fn test_split_volume_records() {
+fn splits_volume_records() {
     let volume = volume::File::new(TEST_NEXRAD_FILE.to_vec());
 
     let records = volume.records().to_vec();
@@ -31,4 +31,20 @@ fn test_split_volume_records() {
             i
         );
     }
+}
+
+#[test]
+fn decodes_volume_header() {
+    let volume = volume::File::new(TEST_NEXRAD_FILE.to_vec());
+
+    let header = volume.header().expect("Failed to parse header");
+
+    let expected_volume_datetime = chrono::DateTime::parse_from_rfc3339("2022-03-05T23:23:24.299Z")
+        .unwrap()
+        .with_timezone(&chrono::Utc);
+
+    assert_eq!(header.tape_filename(), Some("AR2V0006.".to_string()));
+    assert_eq!(header.extension_number(), Some("879".to_string()));
+    assert_eq!(header.date_time(), Some(expected_volume_datetime));
+    assert_eq!(header.icao_of_radar(), Some("KDMX".to_string()));
 }

55fbb81367f02db6ea99452e28c625f1d03c9c78
Daniel Way <projects@danieldway.com>
2025-11-26 16:20:33 +0000

    Convert primitive aliases to zerocopy big-endian types

--- nexrad-decode/src/messages/primitive_aliases.rs
+++ nexrad-decode/src/messages/primitive_aliases.rs
@@ -2,13 +2,15 @@
 //! Primitive aliases matching the types referenced in the ICD.
 //!
 
+use zerocopy::big_endian;
+
 pub type Code1 = u8;
-pub type Code2 = u16;
+pub type Code2 = big_endian::U16;
 pub type Integer1 = u8;
-pub type Integer2 = u16;
-pub type Integer4 = u32;
-pub type Real4 = f32;
+pub type Integer2 = big_endian::U16;
+pub type Integer4 = big_endian::U32;
+pub type Real4 = big_endian::F32;
 pub type ScaledInteger1 = u8;
-pub type ScaledInteger2 = u16;
-pub type ScaledSInteger2 = i16;
-pub type SInteger2 = i16;
+pub type ScaledInteger2 = big_endian::U16;
+pub type ScaledSInteger2 = big_endian::I16;
+pub type SInteger2 = big_endian::I16;

cf88a177bf74b40ba6670ec8cca7f0e0831eda88
Daniel Way <projects@danieldway.com>
2025-12-28 14:03:03 -0600

    Remove old bincode error type

--- nexrad-decode/src/result.rs
+++ nexrad-decode/src/result.rs
@@ -10,8 +10,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     #[error("data file IO error")]
     FileError(#[from] std::io::Error),
-    #[error("file deserialization error")]
-    DeserializationError(#[from] bincode::Error),
     #[error("file decoding error: {0}")]
     DecodingError(String),
     #[error("message is missing collection date/time")]
